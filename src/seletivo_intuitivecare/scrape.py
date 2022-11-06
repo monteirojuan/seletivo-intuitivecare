d2bd0af93f051611e475976208f792cd4584c8c3
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-11-06 17:24:32 -0300

    teste 1: faz download na pasta public/
    Separa a escrita dos arquivos e verifica a existência da pasta 'public/'

--- src/seletivo_intuitivecare/scrape.py
+++ src/seletivo_intuitivecare/scrape.py
@@ -2,6 +2,7 @@
 
 Objetivo: baixar anexos (I a IV) e agrupar em um arquivo zip.
 """
+import os
 import re
 
 import requests
@@ -10,17 +11,33 @@ from bs4 import BeautifulSoup
 URL = "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude"
 
 
-def download():
+def scrape():
+    """Realiza o objetivo do teste 1."""
+    path = find_download_foder()
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     for link in soup.find_all(name="a", href=True, text=re.compile("Anexo")):
         file_name = link["href"].split("/")[-1]
-        file = requests.get(link["href"], stream=True)
-        with open(file_name, 'wb') as fd:
-            for chunk in file.iter_content(chunk_size=256):
-                fd.write(chunk)
+        request = requests.get(link["href"], stream=True)
+        save(request, path + file_name)
+
+
+def save(request: requests.Response, file_path: str):
+    """Salva o resultado de uma requisição."""
+    with open(file_path, 'wb') as fd:
+        for chunk in request.iter_content(chunk_size=256):
+            fd.write(chunk)
+
+
+def find_download_foder():
+    """Determina onde salvar os arquivos."""
+    if os.path.exists("public"):
+        return "public/"
+    if os.path.exists("../../public"):
+        return "../../public/"
+    return "./"
 
 
 if __name__ == '__main__':
-    download()
+    scrape()
