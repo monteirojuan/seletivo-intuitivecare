6ada1c52c939fd3ca8eeea0665cd41329c0db4db
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-11-06 17:43:30 -0300

    teste 1: agrupa arquivos em teste1.zip

--- src/seletivo_intuitivecare/scrape.py
+++ src/seletivo_intuitivecare/scrape.py
@@ -4,6 +4,7 @@ Objetivo: baixar anexos (I a IV) e agrupar em um arquivo zip.
 """
 import os
 import re
+from zipfile import ZipFile
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,14 +14,18 @@ URL = "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-sau
 
 def scrape():
     """Realiza o objetivo do teste 1."""
-    path = find_download_foder()
+    path = find_download_path()
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
+    files: list[str] = []
 
     for link in soup.find_all(name="a", href=True, text=re.compile("Anexo")):
-        file_name = link["href"].split("/")[-1]
+        file_name = path + link["href"].split("/")[-1]
         request = requests.get(link["href"], stream=True)
-        save(request, path + file_name)
+        save(request, file_name)
+        files.append(file_name)
+
+    zip_files(files)
 
 
 def save(request: requests.Response, file_path: str):
@@ -30,7 +35,14 @@ def save(request: requests.Response, file_path: str):
             fd.write(chunk)
 
 
-def find_download_foder():
+def zip_files(files: list[str]):
+    """Agrupa arquivos baixados em 'teste1.zip'"""
+    with ZipFile(find_download_path() + "teste1.zip", "w") as zip_:
+        for file in files:
+            zip_.write(file)
+
+
+def find_download_path():
     """Determina onde salvar os arquivos."""
     if os.path.exists("public"):
         return "public/"
